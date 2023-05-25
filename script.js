e96576b175a011ae128916630c420204c8310cd5
FedericoRosso97 <federicorosso97@gmail.com>
2023-05-25 16:33:26 +0200

    creo funzione che mi sottragga due date

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const lesson=new Date(2023,4,26,09,30);
 console.log(lesson);
-//let today=new Date(2023,4,25,16,30,0,0);
-//let timeNow=Date.toLocaleString();
-//console.log(today.toLocaleString());
 
 let today = new Date();
 let now = today.toLocaleString();
 console.log(now);
+
+let Countdown=countDown(today,lesson)
+console.log(Countdown);
+
+function countDown(date1,date2){
+    let End=date1-date2
+    return End;
+}
