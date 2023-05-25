fe0f4ac0efec33b75ef11b884c6d4ff81ae29f6b
FedericoRosso97 <federicorosso97@gmail.com>
2023-05-25 16:15:32 +0200

    creo variabile che mi restituisce data corrente

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-lesson=new Date(2023,4,26,09,30)
+const lesson=new Date(2023,4,26,09,30);
 console.log(lesson);
-//console.log(new Date(2023,5,25,16).toString());
+//let today=new Date(2023,4,25,16,30,0,0);
+//let timeNow=Date.toLocaleString();
+//console.log(today.toLocaleString());
+
+let today = new Date();
+let now = today.toLocaleString();
+console.log(now);
